5c85b2514cc0b9cd3d195b044b1cd9a6614b5bb8
Vladimir <vovas11@gmail.com>
2017-10-17 18:42:29 +0200

    Add tvShow URL

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const tvShowRouter = require('./tvShow/tvShowRouter');
+
 const app = express();
 const port = 3000;
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/tvshow', tvShowRouter);
 
 app.get('/', (req, res) => res.send('Hello World'));
 
